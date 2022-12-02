93c77ee84b478a30bbd55e2a66055ec8edb39093
denizboy <46217600+denizboy@users.noreply.github.com>
2022-12-03 01:05:04 +0400

    first commit

---
+++ Data/ContactsAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactsAPI1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI1.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
